de7ae2ba576e76f35a37c5cc54b4118167d35f2f
TruthHun <TruthHun@QQ.COM>
2019-08-21 21:35:05 +0800

    组件（TODO）

---
+++ comments/search.vue
@@ -0,0 +1,44 @@
+<template>
+	<form class='search'>
+	  <input 
+		  :focus='focus'
+		  :value='wd' 
+		  :placeholder='placeholder'
+		  @input="changeValue"
+		  @confirm="search"
+		  name='wd' 
+		  type='text'
+	   />
+	  <image bindtap='clear' v-if='showClear' class='clear' src='../static/images/clear.png'></image>
+	  <image bindtap='formSubmit' src='../static/images/search.png'></image>
+	</form>
+</template>
+
+<script>
+	export default{
+		name:"search",
+		data: {
+			showClear: false
+		},
+		props:{
+			focus:{
+				type: Boolean,
+				default: false
+			},
+			wd: {
+				type: String,
+				default: ''
+			},
+			placeholder: {
+				type: String,
+				default: 'Search...'
+			}
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ utils/api.js
@@ -0,0 +1,71 @@
+// const config = require('../config.js')
+const config = import('../config.js')
+const util = require('./util.js')
+
+const keyCategories = 'categories';
+const keyCacheExpire = 'categories-expire';
+const expire = 600; // 600 second
+
+// 获取书籍分类
+const getCategories = () => {
+  if (config.debug) console.log(config.api.categories);
+  // 从缓存中读取，判断缓存存不存在，并且有没有过期
+  return new Promise((resolve, reject) => {
+    let categories = {};
+    let now = util.now();
+    let value
+    let cacheExpire = parseInt(uni.getStorageSync(keyCacheExpire))
+    if (cacheExpire > now) {
+      try {
+        value = uni.getStorageSync(keyCategories)
+        if (value) value = JSON.parse(value)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    if (value) {
+      if (config.debug) console.log("从缓存中获取分类数据");
+      resolve(value);
+    } else {
+      if (config.debug) console.log("从接口获取数据");
+      util.request(config.api.categories).then(function(res) {
+        let tree = util.menuToTree(res.data.categories)
+        uni.setStorageSync(keyCategories, JSON.stringify(tree))
+        uni.setStorageSync(keyCacheExpire, now + expire)
+        resolve(tree)
+      }).catch(function(e) {
+        reject(e)
+      })
+    }
+  });
+}
+
+const getCategoryByCid = (cid) => {
+  return getCategories().then((categories) => {
+    let found = false
+    let category = {}
+    for (let i = 0; found == false && i < categories.length; i++) {
+      if (cid == categories[i].id) {
+        found = true
+        category = categories[i]
+      } else if (categories[i].children.length>0){
+        let children = categories[i].children
+        for (let i = 0; found == false && i < children.length; i++) {
+          if (cid == children[i].id) {
+            found = true
+            category = children[i]
+          }
+        }
+      }
+    }
+    return category
+  })
+}
+
+
+
+module.exports = {
+  getCategories,
+  getCategoryByCid,
+}

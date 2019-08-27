1d660f4f8c91293223ea9ec111fc2c26b6e69f67
TruthHun <TruthHun@QQ.COM>
2019-08-27 21:17:34 +0800

    完成页面

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ BookChat uniapp 版
 
 - [x] 首页 index
 - [x] 分类页 cate
-- [ ] 书架页 bookshelf
+- [x] 书架页 bookshelf(`待验证`)
 - [x] 列表页 list
 - [ ] 用户中心页 ucenter
 - [ ] 个人主页 me
@@ -16,8 +16,8 @@ BookChat uniapp 版
 - [ ] 书籍阅读页 read
 - [x] 书签页 bookmarks(`待验证`)
 - [ ] 搜索结果页 search
-- [ ] 内容点评页 comment
+- [x] 内容点评页 comment(`待验证`)
 - [ ] 书籍介绍页 intro
-- [ ] 404页 notfound
+- [x] 404页 notfound
 - [x] 登录页 login
 - [x] 注册页 register(`待验证`)
--- pages/notfound/notfound.vue
+++ pages/notfound/notfound.vue
@@ -1,22 +1,36 @@
 <template>
 	<view>
-		<text>TODO: not found</text>
+	  <view>
+	    <image src='../../static/images/404.png'></image>
+	  </view>
+	  <text class='font-lv1'>404</text>
+	  <text>确认过眼神，没找到对的人</text>
+	  <view class='pdt-60upx text-center go-home' @click='goHome'>
+	    <image src='../../static/images/home-selected.png'></image>
+	    <text class='color-green'>朕要肥家</text>
+	  </view>
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {
-				
-			}
+			return {}
 		},
 		methods: {
-			
+			goHome: function(e) {
+				uni.switchTab({
+				  url: '/pages/index/index',
+				})
+			}
 		}
 	}
 </script>
 
 <style>
-
+image{width: 60%;margin:80upx auto;display: block;}
+text.font-lv1{font-size: 60upx !important;color: #353535;}
+text{display: block;text-align: center;color: #888888;margin: 30upx 0;}
+.go-home{color: #353535}
+.go-home image{width: 160upx;height: 160upx;margin-bottom: 0;}
 </style>

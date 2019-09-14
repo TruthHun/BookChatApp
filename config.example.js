324b26097bbbf34ed12c1fece71e9b2dad924f7c
TruthHun <TruthHun@QQ.COM>
2019-09-14 17:01:32 +0800

    fix

--- config.example.js
+++ config.example.js
@@ -5,12 +5,12 @@ let host = 'http://localhost:8181/bookchat'
 const debug = true
 
 // 横幅图片比例，宽高比，小数或者比例(后期从后端接口获取)
-const bannerRatio = 825/315
+const bannerRatio = 825 / 315
 
 // 程序信息，置空则不显示
 const info = {
 	// "关于我们"的文档标识
-	about = 'help/bookchat'
+	about: 'help/bookchat',
 	version: 'v1.0',
 	copyright: 'BookStack.CN',
 	license: 'Apache 2.0',
@@ -18,39 +18,39 @@ const info = {
 }
 
 const api = {
-  banners: `${host}/api/v1/banners`, 
-  register: `${host}/api/v1/register`, 
-  login: `${host}/api/v1/login`,
-  loginByWechat: `${host}/api/v1/login-by-wechat`,
-  loginBindWechat: `${host}/api/v1/login-bind-wechat`,
-  loginedBindWechat: `${host}/api/v1/login-bind-wechat`,
-  logout: `${host}/api/v1/logout`,
-  categories: `${host}/api/v1/book/categories`,
-  bookLists: `${host}/api/v1/book/lists`,
-  bookListsByCids: `${host}/api/v1/book/lists-by-cids`,
-  bookInfo: `${host}/api/v1/book/info`,
-  bookMenu: `${host}/api/v1/book/menu`,
-  bookDownload: `${host}/api/v1/book/download`,
-  searchBook: `${host}/api/v1/search/book`,
-  searchDoc: `${host}/api/v1/search/doc`,
-  bookmark: `${host}/api/v1/book/bookmark`,
-  read: `${host}/api/v1/book/read`,
-  userInfo: `${host}/api/v1/user/info`,
-  userRelease: `${host}/api/v1/user/release`,
-  userFans: `${host}/api/v1/user/fans`,
-  userFollow: `${host}/api/v1/user/follow`,
-  bookshelf: `${host}/api/v1/user/bookshelf`,
-  bookStar: `${host}/api/v1/book/star`,
-  comment: `${host}/api/v1/book/comment`,
-  bookRelated: `${host}/api/v1/book/related`,
-  changeAvatar: `${host}/api/v1/user/change-avatar`,
-  changePassword: `${host}/api/v1/user/change-password`
+	banners: `${host}/api/v1/banners`,
+	register: `${host}/api/v1/register`,
+	login: `${host}/api/v1/login`,
+	loginByWechat: `${host}/api/v1/login-by-wechat`,
+	loginBindWechat: `${host}/api/v1/login-bind-wechat`,
+	loginedBindWechat: `${host}/api/v1/login-bind-wechat`,
+	logout: `${host}/api/v1/logout`,
+	categories: `${host}/api/v1/book/categories`,
+	bookLists: `${host}/api/v1/book/lists`,
+	bookListsByCids: `${host}/api/v1/book/lists-by-cids`,
+	bookInfo: `${host}/api/v1/book/info`,
+	bookMenu: `${host}/api/v1/book/menu`,
+	bookDownload: `${host}/api/v1/book/download`,
+	searchBook: `${host}/api/v1/search/book`,
+	searchDoc: `${host}/api/v1/search/doc`,
+	bookmark: `${host}/api/v1/book/bookmark`,
+	read: `${host}/api/v1/book/read`,
+	userInfo: `${host}/api/v1/user/info`,
+	userRelease: `${host}/api/v1/user/release`,
+	userFans: `${host}/api/v1/user/fans`,
+	userFollow: `${host}/api/v1/user/follow`,
+	bookshelf: `${host}/api/v1/user/bookshelf`,
+	bookStar: `${host}/api/v1/book/star`,
+	comment: `${host}/api/v1/book/comment`,
+	bookRelated: `${host}/api/v1/book/related`,
+	changeAvatar: `${host}/api/v1/user/change-avatar`,
+	changePassword: `${host}/api/v1/user/change-password`
 }
 
 module.exports = {
-  api,
-  debug,
-  bannerRatio,
-  about,
-  info,
+	api,
+	debug,
+	bannerRatio,
+	about,
+	info,
 }

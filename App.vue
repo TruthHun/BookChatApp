69c2d10cfdfd258c366a0ab1da39f30fa6b51de6
TruthHun <TruthHun@QQ.COM>
2019-10-20 10:27:14 +0800

    APP版本升级提示

--- App.vue
+++ App.vue
@@ -8,14 +8,57 @@
 		},
 		onLaunch: function() {
 			let info = uni.getSystemInfoSync()
+			info.versionCode = 0
+			info.version = "v1.0.0"
+
+			// #ifdef APP-PLUS
+			// 查询版本信息
+			plus.runtime.getProperty(plus.runtime.appid, function(wgtInfo) {
+				info.versionCode = wgtInfo.versionCode
+				info.version = wgtInfo.version
+
+				// 如果是Android平台，则检测是否有新版本
+				if (String(info.platform).toLowerCase() == "android") {
+					util.request(config.api.lastestVersion).then(function(res) {
+						if (config.debug) console.log("latest version:", res)
+						if (res && res.data && res.data.version) {
+							if (config.debug) console.log("当前版本", info.versionCode, "最新版本", res.data.version)
+							if (info.versionCode < res.data.version) {
+								uni.showModal({
+									title: "高能预警",
+									content: "发现新版本APP，您是否要升级体验？",
+									cancelText: "暂时忽略",
+									confirmText: "码上升级",
+									success: (action) => {
+										if (action.confirm) {
+											if (config.debug) console.log('用户点击确定');
+											plus.runtime.openURL(res.data.url)
+										} else if (res.cancel) {
+											if (config.debug) console.log('用户点击取消');
+										}
+									}
+								})
+							}
+						}
+					}).catch(function(e) {
+						console.log(e)
+					})
+				}
+			});
+			// #endif
+
+
+
 			if (config.debug) console.log(info)
+
 			util.setSysInfo(info)
+
 		},
 		onShow: function() {
-			if(config.debug) console.log('App Show')
+			if (config.debug) console.log('App Show')
 		},
 		onHide: function() {
-			if(config.debug) console.log('App Hide')
+			if (config.debug) console.log('App Hide')
 		}
 	}
 </script>
